735bf9821630da4dfc25521f91f8e8dec836616d
izaak svec <slavi.svec@gmail.com>
2021-01-25 17:42:16 +1100

    Hard coded movies in App.js and styled in css file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import MoviesList from './components/MoviesList'
 
+const App = () => {
+	const [movies, setMovies] = useState([  {
+            "Title": "Inception",
+            "Year": "2010",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg"
+    },
+    {
+            "Title": "Inception",
+            "Year": "2010",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg"
+    },
+    {
+        "Title": "Inception",
+        "Year": "2010",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg"
+    }])
 
-const  App = ()  => {
-  return (
-    <div className="App">
+        return (
+          <div className='container-fluid movies-app-view'>
+            <div className='row'>
+              <MoviesList movies={movies} />
+            </div>
+          </div>
+        )
+    }
 
-    </div>
-  );
-}
-
-export default App;
-
-
+      export default App
 
